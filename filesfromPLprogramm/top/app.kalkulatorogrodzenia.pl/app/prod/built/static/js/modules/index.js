dcb4e38855c33016337db4420b14b36bd6f6a923
PashaDe <139534751+PashaDe@users.noreply.github.com>
2025-09-01 06:51:55 +0300

    Create index.js

---
+++ filesfromPLprogramm/top/app.kalkulatorogrodzenia.pl/app/prod/built/static/js/modules/index.js
@@ -0,0 +1,18 @@
+import Notices from './assets/Notices';
+import Dialog from './assets/Dialog';
+import Popup from './assets/Popup';
+
+import Main from './Main';
+import Users from './Users';
+import Project from './Project';
+
+
+export default [
+	Notices,
+	Dialog,
+	Popup,
+
+	Main,
+	Users,
+	Project,
+];
